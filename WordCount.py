223f58033b95f318017e8c72eb75aae62d7256a4
psadeghian <Pasha@Pashas-Mac.local>
2016-11-18 19:07:01 -0500

    Made some changes to exclude digits and punctuations. The code coverts text to lowercase.

--- WordCount.py
+++ WordCount.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
+import re, string
 try:
     user_input = input("Please type the text file path: ").strip()
-    file=open(user_input,"r+")
+    file = open(user_input, "r+")
     word_count = {}
-    for word in file.read().split():
+    text_in = file.read().lower()
+    exclude = string.punctuation + string.digits
+    text_out = re.sub('['+exclude+']', '', text_in)
+
+    for word in text_out.split():
         # note .split() splits by whitespace
         if word not in word_count:
             word_count[word] = 1
         else:
             word_count[word] += 1
     for key,value in word_count.items():
-        print(key + " : " + value)
+        print(key + " : " + str(value))
 except FileNotFoundError:
     print("File was not found.")
