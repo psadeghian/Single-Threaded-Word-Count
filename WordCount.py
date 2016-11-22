d4bcf40f81708617561c4cd458ef3cd126189f11
psadeghian <Pasha@Pashas-Mac.local>
2016-11-21 21:29:28 -0500

    Added command line input capability. The result is now being written on a file.

--- WordCount.py
+++ WordCount.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
-import re, string
+import re, string, sys
+
+input_file_found = None
 try:
-    user_input = input("Please type the text file path: ").strip()
-    file = open(user_input, "r+")
+    file_in_path = sys.argv[1].strip()
+    # note the first argument is the 'file_name.py'
+    file_in = open(file_in_path, "r")
     word_count = {}
-    text_in = file.read().lower()
+    text_in = file_in.read().lower()
+    file_in.close()
     exclude = string.punctuation + string.digits
     text_out = re.sub('['+exclude+']', '', text_in)
 
@@ -14,7 +18,15 @@ try:
             word_count[word] = 1
         else:
             word_count[word] += 1
-    for key,value in word_count.items():
-        print(key + " : " + str(value))
+
+    input_file_found = True
+    file_out_path = sys.argv[2].strip()
+    file_out = open(file_out_path, "w")
+    for key, value in word_count.items():
+        print(key + " : " + str(value), file=file_out)
+    file_out.close()
 except FileNotFoundError:
-    print("File was not found.")
+    if input_file_found:
+        print("Output file path was not found.")
+    else:
+        print("Input file was not found.")
